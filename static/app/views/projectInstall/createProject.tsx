35826dbf7e7d7e2041f31862db7f336035431f67
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-05-22 09:42:12 -0700

    feat(project-creation): Add experimentation to fronetend

--- static/app/views/projectInstall/createProject.tsx
+++ static/app/views/projectInstall/createProject.tsx
@@ -11,7 +11,7 @@ import Input from 'sentry/components/input';
 import * as Layout from 'sentry/components/layouts/thirds';
 import ExternalLink from 'sentry/components/links/externalLink';
 import PlatformPicker from 'sentry/components/platformPicker';
-import {canCreateProject} from 'sentry/components/projects/utils';
+import {canCreateProject} from 'sentry/components/projects/useProjectCreationAccess';
 import TeamSelector from 'sentry/components/teamSelector';
 import categoryList from 'sentry/data/platformCategories';
 import {IconAdd} from 'sentry/icons';
